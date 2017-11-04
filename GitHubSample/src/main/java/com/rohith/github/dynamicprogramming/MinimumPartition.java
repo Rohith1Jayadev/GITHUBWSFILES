72e84b612ba0e80cd731094e5c3335585479ae52
Rohith Jayadev <ron19dev@gmail.com>
2017-11-05 02:22:09 +0400

    Added Minimum Partition Algorithm Under Dynamic Programming

--- GitHubSample/src/main/java/com/rohith/github/dynamicprogramming/MinimumPartition.java
+++ GitHubSample/src/main/java/com/rohith/github/dynamicprogramming/MinimumPartition.java
@@ -1,6 +1,6 @@
-package com.rohith.github;
+package com.rohith.github.dynamicprogramming;
 
-public class SampleAppTwo {
+public class MinimumPartition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
